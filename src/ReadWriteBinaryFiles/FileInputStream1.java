639e34840d3e9326914b8a21f3e9ed81798c7373
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-07-25 13:59:43 +0700

    Update FileInputStream1 class

--- src/ReadWriteBinaryFiles/FileInputStream1.java
+++ src/ReadWriteBinaryFiles/FileInputStream1.java
@@ -1,7 +1,18 @@
 package ReadWriteBinaryFiles;
 
-public class FileInputStream1 {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class FileInputStream1 {
+    public static void main(String[] args) throws IOException {
+        // tao mot doi tuong inputstream luong doc
+        InputStream is =new FileInputStream("src/ReadWriteBinaryFiles/CodeGym");
+        int i = -1;
+        // khi doc ra -1 thi ket thuc luong
+        while((i=is.read()) != -1){
+            System.out.println((char)i);
+        }
+        is.close();
     }
 }
