59c5529b41d16f12e6a6681e795fd9864e81991c
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-07-25 11:38:30 +0700

    Update ReadByteConsole class

--- src/ReadWriteBinaryFiles/ReadByteConsole.java
+++ src/ReadWriteBinaryFiles/ReadByteConsole.java
@@ -1,7 +1,20 @@
 package ReadWriteBinaryFiles;
 
-public class ReadByteConsole {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class ReadByteConsole {
+    public static void main(String[] args) throws IOException {
+        InputStream is = System.in;
+        while(true){
+            System.out.println("Enter a charactor: ");
+            int ch = is.read();
+            if(ch == 'q'){
+                System.out.println("Successfully");
+                break;
+            }
+            is.skip(2);
+            System.out.println("Result: " + (char)ch);
+        }
     }
 }
